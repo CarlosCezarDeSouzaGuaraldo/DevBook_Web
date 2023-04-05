8d98e25d5c73dfc990d55b1102185fbcf55decfe
Carlos Cezar De Souza Guaraldo <66181262+CarlosCezarDeSouzaGuaraldo@users.noreply.github.com>
2023-04-06 10:29:49 +1200

    creating auth requests on web application

--- src/controllers/pages.go
+++ src/controllers/pages.go
@@ -1,7 +1,10 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
+	"web/src/config"
+	"web/src/requests"
 	"web/src/utils"
 )
 
@@ -17,6 +20,10 @@ func LoadSignupScreen(w http.ResponseWriter, r *http.Request) {
 
 // LoadHomePage render the home screen
 func LoadHomePage(w http.ResponseWriter, r *http.Request) {
-	
+
+	url := fmt.Sprintf("%s/publications", config.URL_API)
+	response, err := requests.DoAuthRequest(r, http.MethodGet, url, nil)
+	fmt.Println(response.StatusCode, err)
+
 	utils.ExecuteTemplate(w, "home.html", nil)
 }
